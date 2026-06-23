f04a3b728c42d8cb7e392be8ce0f114c0136009a
Yashwanth9918 <120705228+Yashwanth9918@users.noreply.github.com>
2026-06-23 12:50:49 +0530

    auth and gateway integration

--- auth-service/src/main/java/com/example/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/example/authservice/controller/AuthController.java
@@ -1,4 +1,47 @@
 package com.example.authservice.controller;
 
+import com.example.authservice.dto.LoginRequestDTO;
+import com.example.authservice.dto.LoginResponseDTO;
+import com.example.authservice.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService){
+        this.authService = authService;
+    }
+
+    @Operation(summary = "Login", description = "Authenticate a user and return a JWT token")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+        Optional<String> tokenOptional = authService.authenticate(loginRequestDTO);
+        if (tokenOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        String token = tokenOptional.get();
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+    @Operation(summary = "validate token")
+    @GetMapping("/validate")
+    public ResponseEntity<Void> validateToken(@RequestHeader("Authorization") String token) {
+        //Authorization : Bearer <token>
+
+        if(token == null || !token.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        return authService.validateToken(token.substring(7))
+                ? ResponseEntity.ok().build()
+                : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
 }
--- auth-service/src/main/java/com/example/authservice/dto/LoginRequestDTO.java
+++ auth-service/src/main/java/com/example/authservice/dto/LoginRequestDTO.java
@@ -1,4 +1,32 @@
 package com.example.authservice.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class LoginRequestDTO {
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @NotNull(message = "Email cannot be null")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotNull(message = "Password cannot be null")
+    @Size(min=8, message = "Password must be at least 8 characters long")
+    private String password;
 }
